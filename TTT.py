54e83f50f3a5fa1c167537b6ec241bf5b3e303ea
Hyojo <samohindenis@yandex.ru>
2024-05-24 22:50:11 +0400

    optimise code

--- TTT.py
+++ TTT.py
@@ -1,51 +1,53 @@
 import random
 
-element = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
-
 
 def games():
-    global element
-    player = input("Выберите x или o: ")
-    bot = ""
-    if player == 'x':
-        bot = 'o'
-    elif player == 'o':
-        bot = 'x'
-    else:
-        games()
-    print_maps()
-    while True:
+    element = [" " for _ in range(9)]
+    player = input("Выберите x или o: ").lower()
+
+    if player not in ['x', 'o']:
+        print("Неверный выбор. Попробуйте снова.")
+        return games()
+
+    bot = 'o' if player == 'x' else 'x'
+    print_maps(element)
+
+    while " " in element:
+        player_choose(element, player, True)
+        print_maps(element)
+        if not end_game(element):
+            return
         if " " in element:
-            player_choose(player, True)
-            print_maps()
+            player_choose(element, bot, False)
+            print_maps(element)
             if not end_game(element):
-                break
-        else:
-            print("Ничья")
-            break
-        if " " in element:
-            player_choose(bot, False)
-            print_maps()
-            if not end_game(element):
-                break
+                return
         else:
             print("Ничья")
             break
 
 
-def player_choose(player, check):
-    ch = ''
-    if check:
-        ch = int(input("Выберите ячейку: "))
+def player_choose(element, player, is_human):
+    if is_human:
+        try:
+            ch = int(input("Выберите ячейку: "))
+            if ch not in range(9):
+                raise ValueError
+        except ValueError:
+            print("Неверный выбор. Попробуйте снова.")
+            return player_choose(element, player, is_human)
     else:
         ch = random.randint(0, 8)
+
     if element[ch] == " ":
         element[ch] = player
     else:
-        player_choose(player, check)
+        if is_human:
+            print("Ячейка занята. Попробуйте снова.")
+        player_choose(element, player, is_human)
 
 
-def print_maps():
+def print_maps(element):
     maps = f"""
      {element[0]} | {element[1]} | {element[2]}
     ---+---+---
@@ -56,23 +58,18 @@ def print_maps():
     print(maps)
 
 
-def end_game(elements):
-    for i in range(0, 7, 3):
-        if (element[i] == element[i + 1] == element[i + 2]) and elements[i] != " ":
-            print(f'Выиграли {element[i]}')
+def end_game(element):
+    winn_comb = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # horizontal
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # vertical
+        (0, 4, 8), (2, 4, 6)  # diagonal
+    ]
+
+    for (a, b, c) in winn_comb:
+        if element[a] == element[b] == element[c] and element[a] != " ":
+            print(f'Выиграли {element[a]}')
             return False
-    for i in range(0, 3):
-        if (element[i] == element[i + 3] == element[i + 6]) and elements[i] != " ":
-            print(f'Выиграли {element[i]}')
-            return False
-    if (element[0] == element[4] == element[8]) and elements[4] != " ":
-        print(f'Выиграли {element[4]}')
-        return False
-    elif (element[2] == element[4] == element[6]) and elements[4] != " ":
-        print(f'Выиграли {element[4]}')
-        return False
-    else:
-        return True
+    return True
 
 
 games()
